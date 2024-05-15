cfff5a7a3de0a05453a674b54ca2e4771b87edde
DylanM69 <139176877+DylanM69@users.noreply.github.com>
2024-05-15 15:04:51 -0500

    Add files via upload

---
+++ AppExamen.Entidades/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppExamen.Entidades
+{
+    // Entidad Actor
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+
+        public List<Pelicula>? Peliculas { get; set; } //FK
+    }
+}
---
+++ AppExamen.Entidades/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppExamen.Entidades
+{
+    public class Pelicula
+    {
+        [Key]
+        public int Id { get; set; } //PK
+        public string Titulo { get; set; }
+        public int Año { get; set; }
+        public string Director { get; set; }
+
+        public List<Actor> Actores { get; set; }
+    }
+}
